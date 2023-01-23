bcb98385630ebd2242acd13d6c8a7df5ffcd7091
Foysal Osmany <foysalo@gmail.com>
2023-01-23 22:27:28 +0600

    implemented tracking module

--- src/tracking/tracking.module.ts
+++ src/tracking/tracking.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TrackingController } from './tracking.controller';
 import { TrackingService } from './tracking.service';
+import { DbModule } from 'src/db/db.module';
 
 @Module({
+  imports: [DbModule],
   controllers: [TrackingController],
-  providers: [TrackingService]
+  providers: [TrackingService],
 })
 export class TrackingModule {}
--- src/tracking/tracking.service.ts
+++ src/tracking/tracking.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TrackingService {}
+export class TrackingService {
+  constructor(@Inject('PG_CONNECTION') private conn) {}
+
+  async searchUsersInRadius(radius: number) {
+    const { CENTER_LATITUDE, CENTER_LONGITUDE } = process.env;
+    try {
+      const result = await this.conn.query(
+        `SELECT * from public.user, public.user_tracking
+        WHERE public.user.id = public.user_tracking.user_id
+        AND point(public.user_tracking.lat, public.user_tracking.lng) <@> point(${CENTER_LATITUDE}, ${CENTER_LONGITUDE}) < ${radius}`,
+        [],
+      );
+
+      if (!result?.rows?.length) {
+        return { error: 'No users found' };
+      }
+
+      return result.rows.map(({ first_name, last_name, gender, lat, lng }) => ({
+        firstName: first_name,
+        lastName: last_name,
+        gender,
+        lat,
+        lng,
+      }));
+    } catch (e) {
+      return { error: e.message };
+    }
+  }
+}
